1a24a8c57de47684a8d6952408206a4bebc340db
CITG RSTU <CITG-14@CITG-14.rgups.edu>
2021-12-14 18:59:58 +0300

    almost done with prj-list

--- static/js/projects.js
+++ static/js/projects.js
@@ -5,26 +5,48 @@ document.addEventListener('DOMContentLoaded', ()=> {
     let button = document.getElementById('load-more');
 
     button.addEventListener('click', ()=> {
-        let height = 0;
-        for(let i = 0; i < realized_projects_array.length; ++i)
-            console.log("offsetTop:" + realized_projects_array[i].offsetTop)
+        let height;
         if(!isOpen) {
             isOpen = true;
-            button.innerText = 'HIDE';
+            button.innerText = 'свернуть';
             height = realized_projects_array[realized_projects_array.length - 1].offsetTop + realized_projects_array[realized_projects_array.length - 1].clientHeight + 50;
         } else {
             isOpen = false;
-            button.innerText = 'load more';
-            height = realized_projects_array[0].offsetTop + realized_projects_array[0].clientHeight + 50;
+            button.innerText = 'развернуть';
+            height = realized_projects_array[3].offsetTop + realized_projects_array[0].clientHeight + 50;
         }
-
-        //console.log("height " + height)
         project_wrapper.style.height = height + 'px';
     });
+    let decorationBlur_blue = document.querySelector('.little-description_decoration-blur');
+    let decorationBlur_purple = document.querySelector('.after-prjcts_decoration-blur-purple');
+    window.addEventListener('scroll', function addRemoveBlur() {
+        if(document.documentElement.scrollTop > 5) {
+            decorationBlur_purple.style.opacity = '0';
+            decorationBlur_blue.style.opacity = '0'
 
+        } else {
+            decorationBlur_purple.style.opacity = '1';
+            decorationBlur_blue.style.opacity = '1';
+        }
+    });
 
+    let post_01 = document.querySelector('.post-01');
     let arrowUp = document.querySelector('.arrow-up');
+    window.addEventListener('scroll', function scrollArrow(){
+        if(checkPosition()) {
+            showHideArrow('block')
+        } else {
+            showHideArrow('none')
+        }
+    });
+    function showHideArrow(value) {
+        arrowUp.style.display = value;
+    }
+    function checkPosition() {
+        return document.documentElement.scrollTop > post_01.offsetTop + post_01.clientHeight - 300;
+    }
+
     arrowUp.onclick =  () => {
-        window.scrollTo(0,0);
+        document.documentElement.scrollTo(0,0);
     }
 })
