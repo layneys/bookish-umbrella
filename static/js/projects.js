31e4e7d851211ac2d05a7e8b11c253de68f2f908
CITG RSTU <CITG-14@CITG-14.rgups.edu>
2021-12-15 18:17:13 +0300

    done with project_list

--- static/js/projects.js
+++ static/js/projects.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=> {
+
+    let container = document.querySelector('.container');
+
     let isOpen = false;
     let project_wrapper = document.querySelector('.projects-wrapper');
     let realized_projects_array = document.getElementsByClassName('prj-element-of-array');
@@ -33,18 +36,24 @@ document.addEventListener('DOMContentLoaded', ()=> {
     let post_01 = document.querySelector('.post-01');
     let arrowUp = document.querySelector('.arrow-up');
     window.addEventListener('scroll', function scrollArrow(){
-        if(checkPosition()) {
-            showHideArrow('block')
+        if(document.documentElement.scrollTop > post_01.offsetTop + post_01.clientHeight - 300) {
+            arrowUp.style.opacity = '1';
         } else {
-            showHideArrow('none')
+            arrowUp.style.opacity = '0';
         }
     });
-    function showHideArrow(value) {
-        arrowUp.style.display = value;
-    }
-    function checkPosition() {
-        return document.documentElement.scrollTop > post_01.offsetTop + post_01.clientHeight - 300;
-    }
+    let footer = document.querySelector('.footer')
+    window.addEventListener('scroll', function arrowPosition() {
+        arrowUp.style.position = 'fixed';
+        if(window.pageYOffset > footer.offsetTop - 800) {
+            arrowUp.style.left = container.offsetLeft + container.clientWidth + 20 +  'px';
+            arrowUp.style.bottom =  195  + 'px';
+            //...
+        } else {
+            arrowUp.style.left = container.offsetLeft + container.clientWidth + 20 +  'px';
+            arrowUp.style.bottom =  100  + 'px';
+        }
+    })
 
     arrowUp.onclick =  () => {
         document.documentElement.scrollTo(0,0);
