af8e1b66573c25382b0104e8ca9fbf361431409b
CITG RSTU <CITG-14@CITG-14.rgups.edu>
2021-12-20 17:47:44 +0300

    little slider fix -- nikityau

--- static/js/current-slider.js
+++ static/js/current-slider.js
@@ -18,10 +18,12 @@ let isMouseDown = false;
 
 let videoLink = "/";
 let videoIsThere = false;
+let isMove = false;
 
 sliderInside.addEventListener('click', clickEvent)
 function clickEvent(e) {
     let target = e.target;
+    console.log(e.target);
     if(target.getAttribute('data-video') && !videoIsThere) {
         popUpBlock.style.display = 'flex';
         popUpBlock__video.style.display = 'block';
@@ -38,10 +40,13 @@ function clickEvent(e) {
         popUpBlock__image.children[0].src = target.src;
     }
 
-    if(target.getAttribute('data-image') ||target.getAttribute('data-video')) {
-        sliderInside.style.left = - target.offsetLeft + 'px';
+    if(target.getAttribute('data-image') || target.getAttribute('data-video')) {
+        sliderInside.style.left = - target.parentElement.offsetLeft + 'px';
+        console.log(target.parentElement.offsetLeft)
         borderCheck();
     }
+
+    isMove = true;
 }
 
 sliderInside.addEventListener('touchstart', function touchStart(e) {
@@ -74,6 +79,8 @@ sliderInside.addEventListener('mousedown', function mouseDown(e) {
 sliderInside.addEventListener('mousemove', function mouseMove(e) {
     if(!isMouseDown) return false;
 
+    isMove = true;
+
     childrenOfInsideSlider.forEach(el => el.style.pointerEvents = 'none');
 
     let x = e.pageX;
@@ -81,6 +88,9 @@ sliderInside.addEventListener('mousemove', function mouseMove(e) {
     borderCheck();
 });
 sliderInside.addEventListener('mouseup', function mouseUp(){
+    isMove = false;
+
+
     isMouseDown = false;
     sliderInside.style.cursor = 'grab';
     sliderInside.style.transition = 'all .6s';
@@ -88,6 +98,8 @@ sliderInside.addEventListener('mouseup', function mouseUp(){
     childrenOfInsideSlider.forEach(el => el.style.pointerEvents = 'all');
 });
 sliderInside.addEventListener('mouseleave', function mouseLeave(){
+    //isMove = false;
+
     isMouseDown = false;
     sliderInside.style.cursor = 'grab';
     sliderInside.style.transition = 'all .6s';
@@ -96,14 +108,25 @@ sliderInside.addEventListener('mouseleave', function mouseLeave(){
 })
 
 
-leftButton.addEventListener('click', function leftShift() {
+leftButton.addEventListener("mousedown", function leftShift() {
+    isMove = true;
+
     sliderInside.style.left = Number.parseInt(sliderInside.style.left) + childrenOfInsideSlider[0].clientWidth + 20 + 'px';
     borderCheck();
 });
-rightButton.addEventListener('click', function rightShift(){
+rightButton.addEventListener("mousedown", function rightShift(){
+    isMove = true;
+
     sliderInside.style.left = Number.parseInt(sliderInside.style.left) - childrenOfInsideSlider[0].clientWidth - 20 + 'px';
     borderCheck();
 });
+leftButton.addEventListener("mouseup", mouseUp);
+rightButton.addEventListener("mousedown", mouseUp);
+
+function mouseUp() {
+    isMove = false;
+}
+
 
 function borderCheck() {
     let inner = sliderInside.getBoundingClientRect();
@@ -122,21 +145,30 @@ function borderCheck() {
 
 (()=> {
     setInterval(()=> {
-        sliderInside.style.left = Number.parseInt(sliderInside.style.left) - childrenOfInsideSlider[0].clientWidth - 20 + 'px';
-        if(borderCheck()) {
-            sliderInside.style.left = 0 + 'px';
+        if(!isMove) {
+            if(isMove) return;
+
+            sliderInside.style.left = Number.parseInt(sliderInside.style.left) - childrenOfInsideSlider[0].clientWidth - 20 + 'px';
+            if(borderCheck()) {
+                sliderInside.style.left = 0 + 'px';
+            }
         }
+
+        return;
     }, 3000);
 })();
 
+
 window.addEventListener('click', (e)=> {
     if(e.target.getAttribute('data-video') || e.target.getAttribute('data-image')) return;
-    if(!e.target.getAttribute('data-dont-touch')) {
+    if(e.target.getAttribute('data-touch')) {
         popUpBlock.style.display = 'none';
         popUpBlock__image.style.display = 'none';
         popUpBlock__video.style.display = 'none';
         popUpBlock__image.children[0].src = "";
         popUpBlock__video.children[0].src = videoLink;
+
+        isMove = false;
     }
 })
 
