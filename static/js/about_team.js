14fe72d7b1e029932c68fe059605d4329306836f
CITG RSTU <CITG-14@CITG-14.rgups.edu>
2021-12-21 16:34:52 +0300

    about team done

---
+++ static/js/about_team.js
@@ -0,0 +1,25 @@
+// let container = document.querySelector('.container');
+// let arrowUp = document.querySelector('.arrow-up');
+// let footer = document.querySelector('.footer');
+// window.addEventListener('scroll', function scrollArrow(){
+//     if(document.documentElement.scrollTop > post_01.offsetTop + post_01.clientHeight - 300) {
+//         arrowUp.style.opacity = '1';
+//     } else {
+//         arrowUp.style.opacity = '0';
+//     }
+// });
+// window.addEventListener('scroll', function arrowPosition() {
+//     arrowUp.style.position = 'fixed';
+//     if(window.pageYOffset > footer.offsetTop - 800) {
+//         arrowUp.style.left = container.offsetLeft + container.clientWidth + 20 +  'px';
+//         arrowUp.style.bottom =  195  + 'px';
+//         //...
+//     } else {
+//         arrowUp.style.left = container.offsetLeft + container.clientWidth + 20 +  'px';
+//         arrowUp.style.bottom =  100  + 'px';
+//     }
+// })
+//
+// arrowUp.onclick =  () => {
+//     document.documentElement.scrollTo(0,0);
+// }
