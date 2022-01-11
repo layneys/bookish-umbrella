c7f66a1870e96ad4d39f99680a8791aafe0821a5
CITG RSTU <CITG-14@CITG-14.rgups.edu>
2022-01-11 17:03:54 +0300

    little geader update (hover)

--- static/js/current-page.js
+++ static/js/current-page.js
@@ -1,7 +1,14 @@
-let headerNav = document.querySelector('.header').querySelector('.nav');
+let headerNav = document.querySelector('header').querySelector('ul');
 let nav = headerNav.querySelectorAll('a');
 nav.forEach(link => {
-    if(window.location.href.includes(link.href)) {
-        link.querySelector('div').style.color = '#000';
+    if(window.location.href === link.href) {
+        link.classList.add('current');
     }
 })
+
+if(window.location.href.includes('profile')) {
+    document.querySelector('[href="http://127.0.0.1:8000/projects/"]').classList.add('current');
+}
+if(window.location.href.includes('project')) {
+    document.querySelector('[href="http://127.0.0.1:8000/team/"]').classList.add('current');
+}
